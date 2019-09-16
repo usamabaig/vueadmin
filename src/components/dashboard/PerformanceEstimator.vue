64a949b84d47f70cff82f034f52e5df6b1af010c
Usama <muhammad.baig@linentech.net>
2019-09-16 23:52:58 +0500

    changes

--- src/components/dashboard/PerformanceEstimator.vue
+++ src/components/dashboard/PerformanceEstimator.vue
@@ -6,8 +6,8 @@
                 <v-text-field
                         v-model="cost_per_lead"
                         label="Insert your cost per lead"
+                        @input="estimate"
                         outlined
-                        clearable
                 ></v-text-field>
             </v-col>
 
@@ -15,8 +15,8 @@
                 <v-text-field
                         v-model="offer_price"
                         label="Insert your offer price"
+                        @input="estimate"
                         outlined
-                        clearable
                 ></v-text-field>
             </v-col>
 
@@ -24,36 +24,26 @@
                 <v-text-field
                         v-model="traffic_cost"
                         label="Insert your traffic cost"
+                        @input="estimate"
                         outlined
-                        clearable
                 ></v-text-field>
             </v-col>
 
             <v-col cols="12" sm="6">
                 <v-text-field
-                        v-model="message"
+                        v-model="traffic_cost_to_generate"
                         label="Insert traffic cost to auto generate"
+                        @input="estimate"
                         outlined
-                        clearable
                 ></v-text-field>
             </v-col>
         </v-row>
 
-        <v-card-title>
-            <div class="flex-grow-1"></div>
-            <v-text-field
-                    v-model="search"
-                    append-icon=""
-                    label="Search"
-                    single-line
-                    hide-details
-            ></v-text-field>
-        </v-card-title>
         <v-data-table
                 :headers="headers"
                 :items="performance_estimator"
-                :search="search"
                 :sort-by="['calories', 'fat']"
+                hide-default-footer
                 :sort-desc="[false, true]"
                 multi-sort
                 class="my_style"
@@ -62,15 +52,14 @@
 </template>
 
 <script>
-
-
-
-
-
   export default {
     data () {
       return {
         search: '',
+        cost_per_lead: '',
+        offer_price: '',
+        traffic_cost: '',
+        traffic_cost_to_generate: '',
         headers: [
           {
             text: 'Conversation Rate',
@@ -85,48 +74,70 @@
         ],
         performance_estimator: [
           {
-            conversational_rate: 'Frozen Yogurt',
-            unit: 159,
-            sales: 6.0,
-            gross_profit: 24,
-            cpa: 4.0,
-          
+            conversational_rate: '1.0%',
+            unit: 0,
+            sales: 0.0,
+            gross_profit: 0,
+            cpa: 0.0,
+            conversational_rate_calc: 1.0 / 100,
           },
            {
-            conversational_rate: 'Frozen Yogurt',
-            unit: 159,
-            sales: 6.0,
-            gross_profit: 24,
-            cpa: 4.0,
-          
+            conversational_rate: '2.0%',
+            unit: 0,
+            sales: 0.0,
+            gross_profit: 0,
+            cpa: 0.0,
+            conversational_rate_calc: 2.0 / 100,
           },
            {
-            conversational_rate: 'Frozen Yogurt',
-            unit: 159,
-            sales: 6.0,
-            gross_profit: 24,
-            cpa: 4.0,
-          
+            conversational_rate: '5.0%',
+            unit: 0,
+            sales: 0.0,
+            gross_profit: 0,
+            cpa: 0.0,
+            conversational_rate_calc: 5.0 / 100,
           },
            {
-            conversational_rate: 'Frozen Yogurt',
-            unit: 159,
-            sales: 6.0,
-            gross_profit: 24,
-            cpa: 4.0,
-          
-          },
-           {
-            conversational_rate: 'Frozen Yogurt',
-            unit: 159,
-            sales: 6.0,
-            gross_profit: 24,
-            cpa: 4.0,
-          
+            conversational_rate: '10.0%',
+            unit: 0,
+            sales: 0.0,
+            gross_profit: 0,
+            cpa: 0.0,
+            conversational_rate_calc: 10.0 / 100,
           },
         ],
       }
     },
+    methods: {
+      estimate() {
+        this.traffic_cost_to_generate = (this.traffic_cost / this.cost_per_lead).toFixed(5);
+
+        var first_row = this.performance_estimator[0];
+        var second_row = this.performance_estimator[1];
+        var third_row = this.performance_estimator[2];
+        var fourth_row = this.performance_estimator[3];
+
+        first_row.unit = (this.traffic_cost_to_generate * first_row.conversational_rate_calc).toFixed(5);
+        second_row.unit = (this.traffic_cost_to_generate * second_row.conversational_rate_calc).toFixed(5);
+        third_row.unit = (this.traffic_cost_to_generate * third_row.conversational_rate_calc).toFixed(5);
+        fourth_row.unit = (this.traffic_cost_to_generate * fourth_row.conversational_rate_calc).toFixed(5);
+
+        first_row.sales = (first_row.unit * this.offer_price).toFixed(5);
+        second_row.sales = (second_row.unit * this.offer_price).toFixed(5);
+        third_row.sales = (third_row.unit * this.offer_price).toFixed(5);
+        fourth_row.sales = (fourth_row.unit * this.offer_price).toFixed(5);
+
+        first_row.gross_profit = (first_row.sales - this.traffic_cost).toFixed(5);
+        second_row.gross_profit = (second_row.sales - this.traffic_cost).toFixed(5);
+        third_row.gross_profit = (third_row.sales - this.traffic_cost).toFixed(5);
+        fourth_row.gross_profit = (fourth_row.sales - this.traffic_cost).toFixed(5);
+
+        first_row.cpa = (this.traffic_cost / (first_row.unit)).toFixed(5);
+        second_row.cpa = (this.traffic_cost / (second_row.unit)).toFixed(5);
+        third_row.cpa = (this.traffic_cost / (third_row.unit)).toFixed(5);
+        fourth_row.cpa = (this.traffic_cost / (fourth_row.unit)).toFixed(5);
+      },
+    }
     
   }
 </script>
